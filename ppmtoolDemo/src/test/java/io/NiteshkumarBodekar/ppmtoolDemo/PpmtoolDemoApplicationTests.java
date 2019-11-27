4e7abfc756891d007a715a78df8ddebda2ae1b83
Nitesh Bodekar <niteshbodekar16@gmail.com>
2019-11-27 18:29:56 +0530

    first commit

---
+++ ppmtoolDemo/src/test/java/io/NiteshkumarBodekar/ppmtoolDemo/PpmtoolDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.NiteshkumarBodekar.ppmtoolDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
